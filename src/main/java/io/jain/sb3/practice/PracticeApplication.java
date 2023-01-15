44a517eb3bc677f5e6ca889c1eaf0836ec42b970
ejaprja <ejaprja@ejaprja-VirtualBox>
2023-01-15 13:54:52 +0000

    Initial setup.

---
+++ src/main/java/io/jain/sb3/practice/PracticeApplication.java
@@ -0,0 +1,13 @@
+package io.jain.sb3.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
